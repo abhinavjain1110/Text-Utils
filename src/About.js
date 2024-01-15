dd386ab9770fd773e63b2387e5dc3f72cc20b91f
Abhinav Jain <abhinavjain11102004@gmail.com>
2024-01-15 22:10:14 +0530

    Text-Utils

--- src/About.js
+++ src/About.js
@@ -44,7 +44,7 @@ export default function About(props) {
         <div className="accordion-item">
         <h2 className="accordion-header">
         <button className="accordion-button collapsed" style={myStyle}type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-            <strong>Browser Comoatible</strong>
+            <strong>Browser Compatible</strong>
         </button>
         </h2>
         <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
